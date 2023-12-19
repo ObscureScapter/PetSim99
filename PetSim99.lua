859c9c60f4eab67755981e080500b59da71b3857
Scapter <slyhades66@gmail.com>
2023-12-19 05:47:53 -0500

    Update PetSim99.lua

--- PetSim99.lua
+++ PetSim99.lua
@@ -149,7 +149,7 @@ for _,v in LibraryModule.Items.All.Globals.All() do
 	elseif table.find(RealFruitNames, RealName) and not FruitIDs[RealName] then
 		FruitIDs[RealName] = RealID
 
-		table.insert(SettingsOrder[4], {"Auto Eat "..RealName, false})
+		table.insert(SettingsOrder[4][2], {"Auto Eat "..RealName, false})
 		table.insert(FruitOrder, RealName)
 	end
 end
