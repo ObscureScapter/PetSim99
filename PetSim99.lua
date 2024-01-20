390f1542d5cba4047a7e044c2a8206649abb20cd
Scapter <slyhades66@gmail.com>
2024-01-20 17:33:29 -0500

    Update PetSim99.lua

--- PetSim99.lua
+++ PetSim99.lua
@@ -253,7 +253,7 @@ local function BuildUI()
 					table.sort(FakeTable, function(a, b) return a[1] > b[1] end)
 					
 					for i = #FakeTable, 1, -1 do
-						table.insert(FakeTable, FakeTable[i][2])
+						table.insert(MyTable, FakeTable[i][2])
 					end
 				elseif q == "Selected Flag" then
 					for _,v in ReplicatedStorage.__DIRECTORY.MiscItems.Flags:GetChildren() do
